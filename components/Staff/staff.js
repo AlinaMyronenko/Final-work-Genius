138c5551370a5dc5dbc2cbe820e8c9de02aa757a
alina myronenko <alinamyronenko93@gmail.com>
2023-07-03 23:36:56 +0300

    two

--- components/Staff/staff.js
+++ components/Staff/staff.js
@@ -1,6 +1,39 @@
 const ROOT_STAFF = document.getElementById('card-staff');
 
 class CharactersStaff {
+    hover( fullinfo, name) {
+        let id = name;
+        let full = '';
+            CHARACTERS.forEach(({name, alternateNames, species, gend, house, dateOfBirth, yearOfBirth, wizard, ancentry, eyeColour, hairColour, wand, patronus, hogwartsStudent, hogwartsStaff, actor, alive,}) => {
+                if (id == name) {
+                 full += `
+                <nav class="fullinfo"><div>
+                <p>Name: <span> ${name}</span></p>
+                <p>Alternate names: <span>${alternateNames}</span></p>
+                <p>Species:<span> ${species}</span></p>
+                <p>Gend:<span> ${gend}</span></p>
+                <p>House: <span>${house}</span></p>
+                <p>Date of birth:<span> ${dateOfBirth}</span></p>
+                <p>Year of birth:<span> ${yearOfBirth}</span></p>
+                <p>Wizard:<span> ${wizard}</span></p>
+                <p>Ancentry:<span> ${ancentry}</span></p>
+                <p>EyeColour:<span> ${eyeColour}</span></p>
+                <p>HairColour:<span> ${hairColour}</span></p>
+                <p>Wand:<span> ${wand}</span></p>
+                <p>Patronus: <span>${patronus}</span></p>
+                <p>Hogwarts student: <span> ${hogwartsStudent}</span></p>
+                <p>Hogwarts staff: <span> ${hogwartsStaff}</span></p>
+                <p>Actor:<span> ${actor}</span></p>
+                <p>Alive:<span> ${alive}</span></p>
+                </div></nav>
+                
+                `;
+                };
+         fullinfo.innerHTML = full;
+               
+            });
+    };
+
     render() {
         let htmlCatalog = '';
         CHARACTERS.forEach(({ name, alternateNames, house, dateOfBirth, img, hogwartsStaff }) => {
@@ -13,7 +46,7 @@ class CharactersStaff {
             <span class="characters-altname">${alternateNames}</span>
             <span class="characters-house">${house}</span>
             <span class="characters-birth">${dateOfBirth}</span>
-            <button class="characters-button">Більше інформації<img src="/img/svg/arrow.svg" alt="..." /></button>
+            <button class="characters-button" onclick="staff.hover(this, '${name}');">Більше інформації<img src="/img/svg/arrow.svg" alt="..." /></button>
             </li>
             `;
             }
--- components/Students/students.js
+++ components/Students/students.js
@@ -1,10 +1,45 @@
 const ROOT_STUDENTS = document.getElementById('card-study');
 
 class Characters {
+    hover( fullinfo, name) {
+        let id = name;
+        let full = '';
+            CHARACTERS.forEach(({name, alternateNames, species, gend, house, dateOfBirth, yearOfBirth, wizard, ancentry, eyeColour, hairColour, wand, patronus, hogwartsStudent, hogwartsStaff, actor, alive,}) => {
+                if (id == name) {
+                 full += `
+                <nav class="fullinfo"><div>
+                <p>Name: <span> ${name}</span></p>
+                <p>Alternate names: <span>${alternateNames}</span></p>
+                <p>Species:<span> ${species}</span></p>
+                <p>Gend:<span> ${gend}</span></p>
+                <p>House: <span>${house}</span></p>
+                <p>Date of birth:<span> ${dateOfBirth}</span></p>
+                <p>Year of birth:<span> ${yearOfBirth}</span></p>
+                <p>Wizard:<span> ${wizard}</span></p>
+                <p>Ancentry:<span> ${ancentry}</span></p>
+                <p>EyeColour:<span> ${eyeColour}</span></p>
+                <p>HairColour:<span> ${hairColour}</span></p>
+                <p>Wand:<span> ${wand}</span></p>
+                <p>Patronus: <span>${patronus}</span></p>
+                <p>Hogwarts student: <span> ${hogwartsStudent}</span></p>
+                <p>Hogwarts staff: <span> ${hogwartsStaff}</span></p>
+                <p>Actor:<span> ${actor}</span></p>
+                <p>Alive:<span> ${alive}</span></p>
+                </div></nav>
+                
+                `;
+                };
+         fullinfo.innerHTML = full;
+               
+            });
+    };
+
     render() {
         let htmlCatalog = '';
+       
         CHARACTERS.forEach(({ name, alternateNames, house, dateOfBirth, img, hogwartsStudent }) => {
             if (hogwartsStudent == true) {
+              
                 htmlCatalog += `
             <li class="characters-el">
             <img class="characters-img" src="${img}"/>
@@ -13,9 +48,10 @@ class Characters {
             <span class="characters-altname">${alternateNames}</span>
             <span class="characters-house">${house}</span>
             <span class="characters-birth">${dateOfBirth}</span>
-            <button class="characters-button">Більше інформації<img src="/img/svg/arrow.svg" alt="..." /></button>
+            <button id="btn" class="characters-button" onclick="stadents.hover(this, '${name}');">Більше інформації<img src="/img/svg/arrow.svg" alt="..." /></button>
             </li>
             `;
+                
             }
             });
 
@@ -24,7 +60,7 @@ class Characters {
         ${htmlCatalog}
         </ul>
         `;
-    
+        
 
         ROOT_STUDENTS.innerHTML = html;
       
